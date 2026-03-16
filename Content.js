e53fa7bf28a5a21b03dafeac56827095e1140e74
Zak ElSeht <zak@shepherd.bio>
2026-03-16 16:33:56 -0400

    Intial commit:  Repo for playing around with custom g-drive editor colors

---
+++ Content.js
@@ -0,0 +1,62 @@
+let colorPalette = [
+    "#323532",
+    "#5A5D56",
+    "#8F8A76",
+    "#BB3524",
+    "#BE4732",
+]
+
+let defaultDrivePallets = [
+    "rgb(248, 250, 253)",
+    "rgb(240, 244, 249)",
+    "rgb(68, 71, 70)"
+]
+
+let colorPaletteMap = {
+    "rgb(248, 250, 253)":"#323532",
+    "rgb(240, 244, 249)":"#5A5D55",
+    "rgb(68, 71, 70)":"#8F8A76",
+}
+
+for (let el of document.querySelectorAll('[class*="kix-appview"]')) {
+    el.style.backgroundColor = colorPalette[Math.floor(Math.random() * 2)];
+}
+
+
+// for (let el of document.querySelectorAll('[class*="docs-"]')) {
+//     el.style.backgroundColor = colorPalette[Math.floor(Math.random() * 5)];
+// }
+
+// for (let el of document.querySelectorAll('[class*="navigation-"]')) {
+//     el.style.backgroundColor = colorPalette[Math.floor(Math.random() * 5)];
+// }
+
+
+// for (let el of document.querySelectorAll('[class*="goog-toolbar-button-outer-box"]')) {
+//     el.style.backgroundColor = colorPalette[Math.floor(Math.random() * 5)];
+// }
+for (let el of document.querySelectorAll('[class*="navigation-"]')) {
+    // el.style.backgroundColor = colorPalette[Math.floor(Math.random() * 2)];
+    if (defaultDrivePallets.includes(el.style.backgroundColor)) {
+        el.style.backgroundColor = colorPaletteMap[el.style.backgroundColor];
+    }
+}
+
+// for (let el of document.querySelectorAll('canvas')) {
+//     // el.style.backgroundColor = colorPalette[2];
+//     el.style.filter = "invert(1) hue-rotate(180deg)";
+
+// }
+
+
+// for (let el of document.querySelectorAll('[class*="kix-page-paginated"]')) {
+//     el.style.backgroundColor = colorPalette[2];
+// }
+
+
+for (let el of document.querySelectorAll('div')) {
+    if (defaultDrivePallets.includes(el.style.backgroundColor)) {
+        el.style.backgroundColor = colorPaletteMap[el.style.backgroundColor];
+    }
+}
+
